67976162ec664e600880be29ffbcd67fadd95cb8
developerdave <dave@developerdave.co.uk>
2015-05-01 20:01:46 +0100

    Initial commit
    Initial commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Dave Green on 01/05/2015.
+//  Copyright (c) 2015 DeveloperDave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
